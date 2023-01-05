10996d64e8c5b78327a3b296a9e7794adb65ef9d
Armand Gillot <>
2023-01-05 23:10:15 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get("/google-reviews", async (req, res, next) => {
   await context.overridePermissions("https://www.google.fr/", ["geolocation"]);
   await page.setGeolocation({ latitude: 45.764043, longitude: 4.835659 });
   await page.goto(`https://www.google.fr/search?q=${search}&hl=fr`);
+  await page.waitForTimeout(3000);
   await page.click("#L2AGLb > div");
   await page.click("span.hqzQac > span > a > span");
   console.log("Lancement du timeout");
